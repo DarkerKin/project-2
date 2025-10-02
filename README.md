c6b81f605eb362023b210cfa1962cabdb7c597d9
astroworld <astroworld@astroworlds-MacBook-Air.local>
2025-10-01 20:30:39 -0400

    finshed coding the project need to do the readme

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ Many companies face the challenge of customers leaving their product for a compe
 For this project, I am using the Iranian Churn Dataset from the UC Irvine Machine Learning Repository (https://archive.ics.uci.edu/dataset/563/iranian+churn+dataset). The dataset contains 3,150 customer records with 13 features, including call failures, complaints, subscription length, charge amount, seconds of use, frequency of use, frequency of SMS, distinct called numbers, age group, service type, and customer value. All attributes are aggregated over the first 9 months, while the churn label reflects the customer’s status at the end of 12 months, leaving a 3-month planning gap.
 
 ## Data Preprocessing
+
+### Handling Categorical Values
+
+Many machine learning algorithms, including Random Forests, work only with numbers, not text. So categorical columns (like "gender": ["Male", "Female"]) need to be converted to numeric form. So, pd.get_dummies converts categorical variables into dummy/indicator variables (One-Hot Encoding)
+
+### Separate Churn
+
+In supervised learning, we need to separate Independent variables that describe the customer (age, usage, complaints, etc) from what we want to predict. Splits the dataset into X where all columns except "churn" and y with "churn" column.
+
+### Train-Test Split
+
+We split into training (80%) and testing (20%) sets so we can evaluate how well the model generalizes to unseen data. we need to ensure that the same churn/non-churn ratio in both train and test sets (important for imbalanced datasets).
+
+## Data visualization
--- model.py
+++ model.py
@@ -1,7 +1,63 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
+
+# convert the csv to pandas dataframe
 df = pd.read_csv("./data/customer_churn.csv")
 
+#Data Preprocessing
+
+#Separating the churn from the dataset
+X = df.drop("Churn", axis=1)   # Features
+y = df["Churn"]                # Target
+
+#Splitting the training and testing datasets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+# Data visualization
+plt.figure(figsize=(15,17))
+sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
+plt.title("Correlation Heatmap")
+plt.savefig("./images/heatmap.png")
+
+
+# Modeling
+
+# Initialize Random Forest
+rf = RandomForestClassifier(
+    n_estimators=100,   # Number of trees
+    max_depth=None,     # Let trees expand until all leaves are pure
+    random_state=42,
+    class_weight="balanced"  # useful if dataset is imbalanced
+)
+
+# Fit model
+rf.fit(X_train, y_train)
+
+y_pred = rf.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
+
+# Get feature importances
+importances = rf.feature_importances_
+features = X.columns
+
+# Sort by importance
+indices = np.argsort(importances)[::-1]
+
+# Plot
+plt.figure(figsize=(10,18))
+plt.title("Feature Importance")
+plt.bar(range(len(importances)), importances[indices], align="center")
+plt.xticks(range(len(importances)), features[indices], rotation=90)
+plt.savefig('./images/random_forester_model.png')
 
